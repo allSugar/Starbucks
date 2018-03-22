53a77fd3cf1c56147f5143eb2531ef35878b0c1b
Sticker <1563772586@qq.com>
2018-03-22 15:18:01 +0800

    增加分类图片尺寸规范

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -7,6 +7,16 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
+  category: any[] = [
+    { url: 'repair-frequency', name: '维修频率' },
+    { url: 'warranty-number', name: '报修次数' },
+    { url: 'repair-maintain', name: '维修分类' },
+    { url: 'easy-hart', name: '易损部位' },
+    { url: 'weekly', name: '每周问题' },
+    { url: 'efficiency', name: '厂商效率' },
+    { url: 'reason', name: '物损原因' },
+    { url: 'remains', name: '遗存问题' }
+  ];
   constructor(public navCtrl: NavController) {
 
   }
