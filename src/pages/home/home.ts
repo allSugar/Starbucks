42394976908abc42699ff87819a8aa8075325626
Sticker <1563772586@qq.com>
2018-03-22 14:34:09 +0800

    update index

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,23 +1,28 @@
 import { Component, ViewChild } from '@angular/core';
 import { NavParams, Slides } from 'ionic-angular';
- 
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
+  category: any[] = [
+    { url: 'repair-order', name: '维修单' },
+    { url: 'cloud-smart', name: '云智能' },
+    { url: 'cloud', name: '云盘' },
+    { url: '3D-panorama', name: '3D全景' },
+    { url: 'store-manage', name: '店面管理' },
+    { url: 'chart', name: '统计' }
+  ];
   @ViewChild(Slides) slides: Slides;
   constructor(
     public navParams: NavParams,
   ) {
-    
+
   }
   ionViewDidLoad() {
     this.slides.autoplayDisableOnInteraction = false;
-    console.log(this.slides);
-    this.slides.pager = true;
-    // this.slides.paginationType = "progress";
   }
 
 }
