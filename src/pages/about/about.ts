912fbbae8e1642dc62342be3edf1e3d93dc2667e
nufang <nu_fang@sina.cn>
2018-03-24 19:00:03 +0800

    add chart children page

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -18,14 +18,7 @@ export class AboutPage {
     { url: 'remains', name: '遗存问题' }
   ];
 
-    problem: any[] = [
-        {url: 'problem_1', num: '12', name: '设计缺陷'},
-        {url: 'problem_2', num: '12', name: '施工缺陷'},
-        {url: 'problem_3', num: '12', name: '自然损耗'},
-        {url: 'problem_4', num: '12', name: '使用不当'},
-        {url: 'problem_5', num: '12', name: '质量问题'},
-        {url: 'problem_6', num: '12', name: '其他原因'}
-    ];
+
 
     import: any[] = [
         {url: 'import_img1'},
