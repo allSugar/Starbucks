60154417a7e26dbe878e4edfbc1321059df5ebd7
nufang <nu_fang@sina.cn>
2018-04-02 15:35:51 +0800

    add cloud-page add slide

--- src/pages/home/cloud-smart/cloud-smart.ts
+++ src/pages/home/cloud-smart/cloud-smart.ts
@@ -15,8 +15,15 @@ export class CloudSmartPage {
   ) {
   }
 
-  ionViewDidLoad() {
-      this.slides.autoplayDisableOnInteraction = false;
+    categorys: object[] = [
+        {url: 'icon_temperature', name: '温度', number: '29℃'},
+        {url: 'icon_humidity', name: '湿度', number: '26%RH'},
+        {url: 'icon_battery', name: '耗电量', number: '40°'}
+    ]
+
+  status: number = 1;
+
+  tabs(n: number) {
+    this.status = n;
   }
-  
 }
