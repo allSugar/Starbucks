489731c7b5f6195bce63657e8c6a8f66602d994d
allSugar <1563772586@qq.com>
2018-04-12 23:48:34 +0800

    增加吧台区内容

--- src/pages/home/store-manage/repair-point/drawing-map/drawing-map.ts
+++ src/pages/home/store-manage/repair-point/drawing-map/drawing-map.ts
@@ -8,7 +8,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class DrawingMapPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  help: Boolean = false;
+  easel: Boolean = false;
   
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams
+  ) {
+  }
+
+  handleFilter(name: string) {
+    if (!name) {
+      this.help = false, this.easel = false;
+      return false;
+    }
+    this[name] = !this[name];
+    name == 'easel' ? this.help = false : this.easel = false;
+  }
 }
